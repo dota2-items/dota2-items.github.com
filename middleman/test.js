a587c295362894242645bec9a30f5c096745fca5
dota2-items <dota2-items@sunyue.org>
2013-11-20 16:41:47 +0800

    middleman

---
+++ middleman/test.js
@@ -0,0 +1,77 @@
+document.write("<script src='jquery-1.10.2.min.js'></s"+"cript>")
+var tabs = Array();
+tabs["76561198070692664"]='big';
+tabs["76561198093727182"]='big';
+tabs["76561198094462805"]='big';
+tabs["76561198069720171"]='big';
+tabs["76561198064686095"]='small';
+tabs["76561198093631883"]='small';
+tabs["76561198078242922"]='small';
+tabs["76561198075686650"]='small';
+tabs["76561198095677527"]='small';
+var indexs = Array();
+indexs["76561198070692664"]=0;
+indexs["76561198093727182"]=1;
+indexs["76561198094462805"]=2;
+indexs["76561198069720171"]=3;
+indexs["76561198064686095"]=0;
+indexs["76561198093631883"]=1;
+indexs["76561198078242922"]=2;
+indexs["76561198075686650"]=3;
+indexs["76561198095677527"]=4;
+function GetandSet()
+{
+alert("1");
+var urls = [" http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=52A32619A2A2E702F8A2E8E9B6D3F2D4&steamids=76561198102413709","http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=52A32619A2A2E702F8A2E8E9B6D3F2D4&steamids=76561198093727182","http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=52A32619A2A2E702F8A2E8E9B6D3F2D4&steamids=76561198094462805","http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=52A32619A2A2E702F8A2E8E9B6D3F2D4&steamids=76561198069720171"];
+$.get(" http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=52A32619A2A2E702F8A2E8E9B6D3F2D4&steamids=76561198070692664,76561198093727182,76561198094462805,76561198069720171,76561198064686095,76561198093631883,76561198078242922,76561198075686650,76561198095677527",function(data){
+for( var i = 0 ; i < 9 ; i++)
+{
+if ( data.response.players[i].hasOwnProperty("gameid"))
+{
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '游戏中';
+}
+else
+{
+
+
+
+switch (data.response.players[i].personastate){
+case 0:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '离线';
+break;
+case 1:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '在线';
+break;
+case 2:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '忙碌';
+break;
+case 3:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '离开';
+break;
+case 4:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '打盹';
+break;
+case 5:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '想交易';
+break;
+case 6:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '想游戏';
+break;
+default:
+document.getElementById(tabs[data.response.players[i].steamid]).rows[indexs[data.response.players[i].steamid]].cells[3].innerText  = '状态未知';
+} 
+} //if
+} //for
+});
+}
+$(document).ready(function()
+{
+setInterval(GetandSet(),60000);
+});
+
+
+
+
+
+
+
